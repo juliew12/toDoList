18032536370ffe7dc10e8aadc5c3f364d11be14c
Julie Wang <wangjulie2506@gmail.com>
2024-07-04 19:26:30 -0400

    Initial Commit

---
+++ toDoList/toDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListApp.swift
+//  toDoList
+//
+//  Created by Julie Wang on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct toDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
